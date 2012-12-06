1ebc45081df513d70e5ca244dca9e72c608bd53a
dreag0nic <spenser.m.bray@gmail.com>
2012-12-06 10:44:20 -0600

    Updated Entity interfaces with graphic support and the z-axis (overridden in a lower class).

--- shakes/entity.h
+++ shakes/entity.h
@@ -10,6 +10,7 @@
 // # Status:        INCOMPLETE
 // # Todo:        * FLESH OUT INTERFACE
 // #              * Pass copy of whatever reference Entity::update() might need
+// #              * Implmement resource manager into sprite loading
 // # Created:       11.22.2012:1508
 // # Last Modified: 12.3.2012:1923
 // #############################################################################
@@ -17,13 +18,18 @@
 #ifndef __SHAKES_ENTITY
 #define __SHAKES_ENTITY
 
+#include <string>
+
 namespace Shakes
 {
 	class Entity
 	{
 		// OBJECT MEMBER DECLARATION --
 		private:
-			int x, y; // position element
+			int x, y, z;             // position element
+			
+			std::string graphicName; // graphic element file name (w/ extension w/o path)
+			std::string graphicPath; // graphic element path (w/o file name) 
 		
 		// OBJECT METHODS --
 		public:
@@ -32,8 +38,16 @@ namespace Shakes
 			virtual ~Engine(void);
 		
 		// -- GENERAL
-			virtual void update(void) = 0; // Updates the object this entity represents.
-			virtual void draw(void) = 0;   // Draws this entity using OpenGL
+			virtual void update(void) = 0;                          // Updates the object this entity represents.
+			virtual void draw(void) = 0;                            // Draws this entity using OpenGL
+			
+			virtual void loadGraphic(std::string fullFilePath) = 0; // Loads the graphic uses the full path, including name and extension.
+		
+		// -- ACCESSOR METHODS
+			void setX(int x) { this->x = x; }
+			void setY(int y) { this->y = y; }
+			void setZ(int z) { this->z = z; }
+			void setPosition(int x, int y, int z) { this->x = x; this->y = y; this->z = z; }
 	}
 }
 
