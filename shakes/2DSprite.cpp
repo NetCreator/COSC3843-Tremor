ea23fa59afc82694d11f7c79b3770c31cf561294
dreag0nic <spenser.m.bray@gmail.com>
2012-12-06 12:30:28 -0600

    ROFLCopter

---
+++ shakes/2DSprite.cpp
@@ -0,0 +1,17 @@
+// #############################################################################
+// # Project:       Shakes Game Engine
+// # File:          2DSprite.cpp
+// # Author:        SpenSer Bray
+// # Purpose:       Implements the 2DSprite class to provide a simple sprite
+// #           object full of nice neat loading and drawing functions.
+// # Status:        INCOMPLETE
+// # Todo:        * Implmement resource manager into sprite loading
+// #              * Add z-buffer value (essentially just the z-axis)
+// #              * Support resource/asset loading
+// #              * ACTUALLY IMPLEMENT
+// # Created:       12.6.2012:1124
+// # Last Modified: 12.6.2012:1124
+// #############################################################################
+
+#include "2DSprite.h"
+
--- shakes/2DSprite.h
+++ shakes/2DSprite.h
@@ -7,20 +7,21 @@
 // #           dragons, unicorns, and pretty butterflies.
 // # Status:        INCOMPLETE
 // # Todo:        * Implmement resource manager into sprite loading
-// #              * Add z-buffer value (essentially just the z-axis)
-// #              * Support resource.asset loading
+// #              * Support resource/asset loading
 // # Created:       12.6.2012:1100
-// # Last Modified: 12.6.2012:1100
+// # Last Modified: 12.6.2012:1127
 // #############################################################################
 
 #ifndef __SHAKES_2DSPRITE
 #define __SHAKES_2DSPRITE
 
+#include <GL/glut.h>
+
 #include "entity.h"
 
 namespace Shakes
 {
-	class 2DSprite: protected Shakes::Entity
+	class 2DSprite: private Shakes::Entity
 	{
 		// OBJECT MEMBER DECLARATION --
 		// OBJECT METHODS --
@@ -36,9 +37,11 @@ namespace Shakes
 		// -- ACCESSOR METHODS
 			void setX(int x) { this->x = x; }
 			void setY(int y) { this->y = y; }
+			void setDepth(int depth) { this->z = depth; } // sneaky bugger, eh? :D
 			void setPosition(int x, int y) { this->x = x; this->y = y; }
 			
 			int getX(void) { return this->x; }
-			int getY(void) { return this->y; }			
+			int getY(void) { return this->y; }
+			int getDepth(void) { return this->z; }
 	}
 }
